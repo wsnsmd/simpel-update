40759afd3c7da7eba9a111cc21b0fe2edd40c272
Wawan Setiawan <wsnsmd@gmail.com>
2026-02-24 11:48:21 +0800

    update perbaikan closure route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ $admin_path = config('app.admin_path');
 Route::get($admin_path . '/login', 'Auth\AuthentikController@loginRedirect')->name('login');
 // Route::post($admin_path . '/login', 'Auth\LoginController@login')->middleware('throttle:5,1');;
 // Route::post($admin_path . '/logout', 'Auth\LoginController@logout')->name('logout');
-// Route::get($admin_path . '/reload-captcha', 'Auth\LoginController@reloadCaptcha')->name('reload.captcha');
+Route::get($admin_path . '/reload-captcha', 'Auth\LoginController@reloadCaptcha')->name('reload.captcha');
 
 // Route::get('/auth/keycloak/redirect', 'Auth\KeycloakAuthController@redirect')->name('keycloak.redirect');
 // Route::get('/auth/keycloak/callback', 'Auth\KeycloakAuthController@callback')->name('keycloak.callback');
