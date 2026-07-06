3e3f95ab5aa57db362fdc423be45b8da890f6380
Wawan Setiawan <wsnsmd@gmail.com>
2026-07-06 10:48:09 +0800

    Update Login Authentik Bug

--- app/Services/AuthentikAuth.php
+++ app/Services/AuthentikAuth.php
@@ -1,19 +1,15 @@
 <?php
-
 namespace App\Services;
-
 use League\OAuth2\Client\Provider\GenericProvider;
-
 class AuthentikAuth
 {
     public static function makeProvider()
     {
-        $baseUrl = rtrim(env('AUTHENTIK_BASE_URL'), '/');
-
+        $baseUrl = rtrim(config('services.authentik.base_url'), '/');
         return new GenericProvider([
-            'clientId' => config('AUTHENTIK_CLIENT_ID'),
-            'clientSecret' => config('AUTHENTIK_CLIENT_SECRET'),
-            'redirectUri' => config('AUTHENTIK_REDIRECT_URI'),
+            'clientId' => config('services.authentik.client_id'),
+            'clientSecret' => config('services.authentik.client_secret'),
+            'redirectUri' => config('services.authentik.redirect'),
             'urlAuthorize' => $baseUrl . '/application/o/authorize/',
             'urlAccessToken' => $baseUrl . '/application/o/token/',
             'urlResourceOwnerDetails' => $baseUrl . '/application/o/userinfo/',
